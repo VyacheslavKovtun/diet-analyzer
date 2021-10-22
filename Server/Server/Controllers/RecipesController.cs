e353565ebd5bcaec1207a36c38bfc6e3de7c69b5
Vyacheslav Kovtun <kovtyn11191@gmail.com>
2021-10-22 20:39:32 +0300

    recipes api controller was created; get recipes request was tested

---
+++ Server/Server/Controllers/RecipesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        static readonly HttpClient client = new HttpClient();
+        string url;
+
+        private string API_KEY;
+
+        public RecipesController()
+        {
+            API_KEY = Environment.GetEnvironmentVariable("SPOONACULAR_API_KEY");
+
+            url = "https://api.spoonacular.com/recipes/random" + "?apiKey=" + API_KEY;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var res = client.GetStringAsync(url).Result;
+            return res;
+        }
+    }
+}
