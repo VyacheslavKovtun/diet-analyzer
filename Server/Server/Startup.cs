a1ec7aeefd565711046c95ec49339facac875950
Vyacheslav Kovtun <kovtyn11191@gmail.com>
2021-10-22 11:09:21 +0300

    identity role was added to startup settings

--- Server/Server/Startup.cs
+++ Server/Server/Startup.cs
@@ -32,7 +32,7 @@ namespace Server
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
 
