3f3ed52c2f8da023873053ee4b02c5500c6887c0
Vyacheslav Kovtun <kovtyn11191@gmail.com>
2021-11-04 19:49:50 +0200

    db sets were added to db context

---
+++ Server/Server.Domain.Interfaces/Interfaces/IRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Domain.Interfaces.Interfaces
+{
+    public interface IRepositoryAsync<TKey, TValue>
+        where TKey: struct
+        where TValue: class
+    {
+        Task CreateAsync(TValue value);
+        Task<IEnumerable<TValue>> GetAllAsync();
+        Task<TValue> GetAsync(TKey id);
+        Task UpdateAsync(TValue value);
+        Task DeleteAsync(TKey id);
+    }
+}
--- Server/Server.Infrastructure.Data/Database/ApplicationDbContext.cs
+++ Server/Server.Infrastructure.Data/Database/ApplicationDbContext.cs
@@ -1,15 +1,35 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Server.Domain.Core.Entities;
 
 namespace Server.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ApiUser> ApiUsers { get; set; }
+
+        public DbSet<BaseInfo> BaseInfo { get; set; }
+        public DbSet<CaloricInfo> CaloricInfo { get; set; }
+
+        public DbSet<ProductBaseInfo> ProductsBaseInfo { get; set; }
+        public DbSet<CurrentProduct> CurrentProducts { get; set; }
+        public DbSet<ForbiddenProduct> ForbiddenProducts { get; set; }
+        public DbSet<ProductStatistic> ProductsStatistic { get; set; }
+        public DbSet<ProductsExpense> ProductsExpenses { get; set; }
+
+        public DbSet<IngredientBaseInfo> IngredientsBaseInfo { get; set; }
+        public DbSet<CurrentIngredient> CurrentIngredients { get; set; }
+        public DbSet<ForbiddenIngredient> ForbiddenIngredients { get; set; }
+        public DbSet<IngredientStatistic> IngredientsStatistic { get; set; }
+        public DbSet<IngredientsExpense> IngredientsExpenses { get; set; }
+
+        public DbSet<RecipeBaseInfo> RecipesBaseInfo { get; set; }
+        public DbSet<FavouriteRecipe> FavouriteRecipes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
             Database.EnsureCreated();
-            /*DbSet<>*/
         }
     }
 }
