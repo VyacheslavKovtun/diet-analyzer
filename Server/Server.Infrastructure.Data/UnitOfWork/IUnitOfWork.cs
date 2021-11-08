ba53e4a2ae712c698ef777d5fa4b4cd9db20247f
Vyacheslav Kovtun <kovtyn11191@gmail.com>
2021-11-08 15:44:47 +0200

    Unit of Work was created

---
+++ Server/Server.Infrastructure.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Server.Infrastructure.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Infrastructure.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ApiUsersRepository ApiUsersRepository { get; }
+        BaseInfoRepository BaseInfoRepository { get; }
+        CaloricInfoRepository CaloricInfoRepository { get; }
+
+        IngredientsBaseInfoRepository IngredientsBaseInfoRepository { get; }
+        CurrentIngredientsRepository CurrentIngredientsRepository { get; }
+        ForbiddenIngredientsRepository ForbiddenIngredientsRepository { get; }
+        IngredientsStatisticRepository IngredientsStatisticRepository { get; }
+        IngredientsExpensesRepository IngredientsExpensesRepository { get; }
+
+        ProductsBaseInfoRepository ProductsBaseInfoRepository { get; }
+        CurrentProductsRepository CurrentProductsRepository { get; }
+        ForbiddenProductsRepository ForbiddenProductsRepository { get; }
+        ProductsStatisticRepository ProductsStatisticRepository { get; }
+        ProductsExpensesRepository ProductsExpensesRepository { get; }
+
+        RecipesBaseInfoRepository RecipesBaseInfoRepository { get; }
+        FavouriteRecipesRepository FavouriteRecipesRepository { get; }
+    }
+}
