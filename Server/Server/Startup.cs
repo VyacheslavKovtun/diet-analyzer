bbceee3e685dc4834857efc4dc3b6e7b0c91f6a2
Vyacheslav Kovtun <kovtyn11191@gmail.com>
2021-11-11 17:52:45 +0200

    transients were added to startup settings

--- Server/Server/Startup.cs
+++ Server/Server/Startup.cs
@@ -9,7 +9,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Server.Data;
+using Server.Infrastructure.Data.UnitOfWork;
 using Server.Services;
+using Server.Services.Interfaces.Interfaces;
+using Server.Services.Interfaces.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,6 +46,26 @@ namespace Server
             services.AddTransient<IEmailSender, EmailSender>();
             services.Configure<AuthMessageSenderOptions>(Configuration);
 
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IApiUsersService, ApiUsersService>();
+            services.AddTransient<IBaseInfoService, BaseInfoService>();
+            services.AddTransient<ICaloricInfoService, CaloricInfoService>();
+
+            services.AddTransient<IIngredientsBaseInfoService, IngredientsBaseInfoService>();
+            services.AddTransient<ICurrentIngredientsService, CurrentIngredientsService>();
+            services.AddTransient<IForbiddenIngredientsService, ForbiddenIngredientsService>();
+            services.AddTransient<IIngredientsStatisticService, IngredientsStatisticService>();
+            services.AddTransient<IIngredientsExpensesService, IngredientsExpensesService>();
+
+            services.AddTransient<IProductsBaseInfoService, ProductsBaseInfoService>();
+            services.AddTransient<ICurrentProductsService, CurrentProductsService>();
+            services.AddTransient<IForbiddenProductsService, ForbiddenProductsService>();
+            services.AddTransient<IProductsStatisticService, ProductsStatisticService>();
+            services.AddTransient<IProductsExpensesService, ProductsExpensesService>();
+
+            services.AddTransient<IRecipesBaseInfoService, RecipesBaseInfoService>();
+            services.AddTransient<IFavouriteRecipesService, FavouriteRecipesService>();
+
             services.AddRazorPages();
 
             services.AddCors();
@@ -52,7 +75,7 @@ namespace Server
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors(options =>
-            options.WithOrigins("http://localhost:4200") //ip of angular project
+            options.WithOrigins("http://localhost:4200")
             .AllowAnyMethod()
             .AllowAnyHeader());
 
